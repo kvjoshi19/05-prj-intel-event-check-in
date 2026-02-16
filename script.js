1f257675f76a613387abe2fa88902ad4b811d62a
Khanak Joshi <kvjoshi@asu.edu>
2026-02-16 23:23:09 +0000

    updates

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
 
+const greeting = document.getElementById("greeting");
+const attendeeCountText = document.getElementById("attendeeCount");
+const progressBar = document.getElementById("progressBar");
+
+const teamIds = {
+  water: "waterCount",
+  zero: "zeroCount",
+  power: "powerCount",
+};
+
 let count = 0;
 const maxCount = 50;
 
@@ -13,19 +23,19 @@ form.addEventListener("submit", function (event) {
   const team = teamSelect.value;
   const teamName = teamSelect.selectedOptions[0].text;
 
-  console.log(name, teamName);
+  if (!name || !team) return;
+  count += 1;
+  attendeeCountText.textContent = count;
 
-  count ++;
-  console.log("Total check-ins: ", count);
-
-  const percentage = Math.round((count / maxCount) * 100) + "%";
-  console.log('Progress: ${percentage}');
+  const percentage = Math.round((count / maxCount) * 100);
+  progressBar.style.width = '${percentage}%';
+  progressBar.setAttribute("aria-valuenow", percentage);
+  progressBar.textContent = `${percentage}%`;
 
   const teamCounter = document.getElementById(team + "Count");
   teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
 
-  const message = `Welcome, ${name} from ${teamName}. You have checked in.`;
-  console.log(message);
+ greeting.textContent = `Welcome, ${name} from ${teamName}. You have checked in.`;
 
   form.reset();
 });
